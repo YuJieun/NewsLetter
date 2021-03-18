a82f8db350efaf6b74cb8415ea7981e8577a4f10
YuJieun <idpresent@naver.com>
2021-03-18 15:15:10 +0900

    제목 : 브랜드필터링구현 내용 :

--- NewsLetter/NewsLetter/Home/filterCell/FilterBrandCell.swift
+++ NewsLetter/NewsLetter/Home/filterCell/FilterBrandCell.swift
@@ -11,20 +11,26 @@ class FilterBrandCell: CommonCollectionViewCell {
 
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var titleView: UIView!
+    
+    var brands: DI_PlatformList?
+    var totalData: DI_Platform?
 
     override func awakeFromNib() {
         super.awakeFromNib()
         self.collectionView.registerNibCell("FilterBrandItemCell", Classs: FilterBrandItemCell.self)
+        self.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "UICollectionViewCell")
     }
 
     func configure(data: Any? = nil) {
-        guard let _ = data else { return }
-
+        guard let data = data as? DI_PlatformList else { return }
+        self.brands = data
+        self.collectionView.reloadData()
     }
 
     class func getSize(_ data: Any? = nil) -> CGSize {
+        guard let data = data as? DI_PlatformList else { return .zero }
         let cell = Self.getXib(className: "FilterBrandCell", as: self)
-        cell.configure()
+        cell.configure(data: data)
         cell.collectionView.reloadData()
         cell.collectionView.layoutIfNeeded()
         let fixedHeight = cell.titleView.h + cell.collectionView.bottomConstraint
@@ -40,20 +46,33 @@ extension FilterBrandCell: UICollectionViewDataSource, UICollectionViewDelegate,
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 8
+        return (self.brands?.resultList.count ?? 0) + 1 //전체 셀 포함
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let arr = ["디독", "뉴미디어", "유지은", "레인보우레인보우아이렇게길면어떻게나오나", "엥","중간길이", "테스트용이지롱", "와우우우우우우"]
+        guard let brands = self.brands else { return collectionView.dequeueReusableCell(withReuseIdentifier: "UICollectionViewCell", for: indexPath) }
         let cell = collectionView.dequeueReusableCell(FilterBrandItemCell.self, "FilterBrandItemCell", for: indexPath)
-        cell.configure(data: arr[indexPath.row])
+        if indexPath.row == 0 {
+            cell.configure(data: totalData)
+        }
+        else {
+            cell.configure(data: brands.resultList[indexPath.row - 1])
+            cell.cellClosure = { [weak self] (type, data) in
+                guard let self = self else { return }
+                self.cellClosure?(type, data)
+            }
+        }
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let arr = ["디독", "뉴미디어", "유지은", "레인보우레인보우아이렇게길면어떻게나오나", "엥","중간길이", "테스트용이지롱", "와우우우우우우"]
-        let size = FilterBrandItemCell.getSize(arr[indexPath.row])
-        return size
+        guard let brands = self.brands else { return .zero }
+        if indexPath.row == 0 {
+            return FilterBrandItemCell.getSize(totalData)
+        }
+        else {
+            return FilterBrandItemCell.getSize(brands.resultList[indexPath.row - 1])
+        }
     }
     
 }
--- NewsLetter/NewsLetter/Home/filterCell/FilterBrandItemCell.swift
+++ NewsLetter/NewsLetter/Home/filterCell/FilterBrandItemCell.swift
@@ -11,15 +11,29 @@ class FilterBrandItemCell: CommonCollectionViewCell {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var brandButton: UIButton!
+    @IBOutlet weak var backView: UIView!
+    
+    var data: DI_Platform?
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        updateUI()
     }
 
     func configure(data: Any? = nil) {
-        guard let data = data as? String else { return }
-        updateUI()
-        self.titleLabel.text = data
+        guard let data = data as? DI_Platform else { return }
+        self.data = data
+        self.titleLabel.text = data.name
+        
+        if data.isSelected ?? false {
+            self.backView.backgroundColor = UIColor(rgb: 0xffdccc)
+            self.brandButton.isSelected = true
+        }
+        else {
+            self.backView.backgroundColor = UIColor(rgb: 0xffffff)
+            self.brandButton.isSelected = false
+        }
     }
     
     func updateUI() {
@@ -29,12 +43,23 @@ class FilterBrandItemCell: CommonCollectionViewCell {
     }
 
     class func getSize(_ data: Any? = nil) -> CGSize {
-        guard let data = data as? String else { return .zero }
+        guard let data = data as? DI_Platform else { return .zero }
         let cell = Self.getXib(className: "FilterBrandItemCell", as: self)
-        let width = data.width(withConstrainedHeight: 34, font: cell.titleLabel.font)
+        let width = data.name.width(withConstrainedHeight: 34, font: cell.titleLabel.font)
         
         return CGSize(width: width + 24, height: 34)
     }
 
-
+    @IBAction func onButton(_ sender: UIButton) {
+        guard let data = self.data, data.name != "전체" else { return }
+        if self.brandButton.isSelected {
+            data.isSelected = false
+            self.cellClosure?("delete", data)
+        }
+        else {
+            data.isSelected = true
+            self.cellClosure?("add", data)
+        }
+    }
+    
 }
